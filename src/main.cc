745a606dfa1f3c2ed9f7bf9af1c22315c2c3fd4a
Robert Peters <robert.peters@fabmatics.com>
2023-11-12 20:50:57 +0100

    Draw texture to improve render performance

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,17 @@
 #include <cstring>
-#include <random>
-#include <raylib.h>
-
 #include <memory>
 #include <random>
+#include <raylib.h>
 #include <span>
 #include <stdint.h>
 #include <string>
 #include <utility>
+
+// #define DEBUG
+
+#ifdef DEBUG
+#include <iostream>
+#endif
 
 constexpr bool is_alive(uint8_t cell) { return cell & 0x80; }
 
@@ -29,6 +33,11 @@ class GOL {
 public:
   GOL(int64_t width, int64_t height) : _rng_gen(std::random_device{}()) {
     resize(width, height);
+  }
+
+  ~GOL() {
+    if (_array)
+      delete[] _array;
   }
 
   int64_t width() const { return _width; }
@@ -56,7 +65,8 @@ public:
   }
 
   void resize(int64_t width, int64_t height) {
-    delete[] _array;
+    if (_array)
+      delete[] _array;
 
     _width = width;
     _height = height;
@@ -98,7 +108,6 @@ private:
 
     array[to_index(wrap(0, width(), x - 1), wrap(0, height(), y), width())] +=
         1;
-    // array[i] += 1;
     array[i] |= 0x80;
     array[to_index(wrap(0, width(), x + 1), wrap(0, height(), y), width())] +=
         1;
@@ -126,7 +135,6 @@ private:
 
     array[to_index(wrap(0, width(), x - 1), wrap(0, height(), y), width())] -=
         1;
-    // array[i] -= 1;
     array[i] &= 0x7f;
     array[to_index(wrap(0, width(), x + 1), wrap(0, height(), y), width())] -=
         1;
@@ -140,15 +148,33 @@ private:
   }
 
 private:
-  int64_t _width;
-  int64_t _height;
+  int64_t _width = 0;
+  int64_t _height = 0;
 
-  uint8_t *_array;
+  uint8_t *_array = 0;
   std::span<uint8_t> _array_curr;
   std::span<uint8_t> _array_next;
 
   std::mt19937 _rng_gen;
 };
+
+Image image_new(int width, int height) {
+  return Image{.data = malloc(width * height * 4),
+               .width = width,
+               .height = height,
+               .mipmaps = 1,
+               .format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8};
+}
+void image_resize(Image &image, int width, int height) {
+  delete[] (char *)image.data;
+  image.data = new char[width * height * 4];
+  image.width = width;
+  image.height = height;
+}
+void texture_reload(Texture &texture, Image &image) {
+  UnloadTexture(texture);
+  texture = LoadTextureFromImage(image);
+}
 
 int main() {
   static_assert(is_alive(0x82), "should be alive");
@@ -164,20 +190,26 @@ int main() {
 
   int64_t tgt_window_width = 800;
   int64_t tgt_window_height = 600;
-  int64_t scalar = 1;
+  int64_t scalar = 2;
 
   bool paused = false;
   GOL gol(tgt_window_width / scalar, tgt_window_height / scalar);
 
+  SetTraceLogLevel(LOG_ERROR);
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(tgt_window_width, tgt_window_height, "yagolcpp");
-  // SetTargetFPS(60);
+
+  Image screen_image = image_new(gol.width(), gol.height());
+  Texture2D screen_texture = LoadTextureFromImage(screen_image);
+  SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
     if (IsWindowResized()) {
       tgt_window_width = GetRenderWidth();
       tgt_window_height = GetRenderHeight();
       gol.resize(tgt_window_width / scalar, tgt_window_height / scalar);
+      image_resize(screen_image, gol.width(), gol.height());
+      texture_reload(screen_texture, screen_image);
     }
     if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
       auto const mouse_pos = GetMousePosition();
@@ -209,35 +241,81 @@ int main() {
     auto const actual_fps = 1.0 / GetFrameTime();
 
     auto const &array = gol.array();
-    auto const width = gol.width();
-    auto const height = gol.width();
 
+#ifdef DEBUG
+    auto const ts_start_image_update = GetTime();
+#endif
+    auto const array_size = array.size();
+    for (std::size_t i = 0; i < array_size; ++i) {
+      static uint32_t const colors[] = {0xFFFFFFFF, 0xFF000000};
+      reinterpret_cast<uint32_t *>(screen_image.data)[i] =
+          colors[(array[i] & 0x80) >> 7];
+    }
+#ifdef DEBUG
+    auto const ts_end_image_update = GetTime();
+#endif
+
+#ifdef DEBUG
+    auto const ts_start_update_texture = GetTime();
+#endif
+    Color *const pixels = LoadImageColors(screen_image);
+    UpdateTexture(screen_texture, pixels);
+    UnloadImageColors(pixels);
+#ifdef DEBUG
+    auto const ts_end_update_texture = GetTime();
+#endif
+
+#ifdef DEBUG
+    auto const ts_start_drawing = GetTime();
+#endif
     BeginDrawing();
     {
-      ClearBackground(RAYWHITE);
-      for (int64_t i = 0; i < array.size(); ++i) {
-        auto const x = i % width;
-        auto const y = i / width;
-        if (array[i] & 0x80)
-          DrawRectangle(x * scalar, y * scalar, scalar, scalar, GRAY);
-      }
+      Rectangle camera{.x = 0,
+                       .y = 0,
+                       .width = (float)gol.width(),
+                       .height = (float)gol.height()};
+      Rectangle screen{.x = 0,
+                       .y = 0,
+                       .width = (float)tgt_window_width,
+                       .height = (float)tgt_window_height};
+      DrawTexturePro(screen_texture, camera, screen, {0, 0}, 0.0f, WHITE);
 
       DrawText(std::to_string(timedelta).c_str(), 10, 10, 20, BLACK);
       DrawText(std::to_string(actual_fps).c_str(), 10, 25, 20, BLACK);
 
       if (paused) {
         DrawText("PAUSED", tgt_window_width / 2,
-                 tgt_window_height / 2 + (std::sin(GetTime()) * 100) - 50, 30,
-                 BLACK);
+                 tgt_window_height / 2.0f + (std::sin(GetTime()) * 100) - 50,
+                 30, BLACK);
       }
     }
     EndDrawing();
+#ifdef DEBUG
+    auto const ts_end_drawing = GetTime();
+#endif
 
+#ifdef DEBUG
+    auto const ts_start_iterate = GetTime();
+#endif
     if (!paused) {
       gol.iterate();
     }
+#ifdef DEBUG
+    auto const ts_end_iterate = GetTime();
+#endif
+
+#ifdef DEBUG
+    std::cout << " image: "
+              << (ts_end_image_update - ts_start_image_update) * 100
+              << " ms texture: "
+              << (ts_end_update_texture - ts_start_update_texture) * 100
+              << " ms draw: " << (ts_end_drawing - ts_start_drawing) * 100
+              << " ms iterate: " << (ts_end_iterate - ts_start_iterate) * 100
+              << " ms" << std::endl;
+#endif
   }
 
+  UnloadImage(screen_image);
   CloseWindow();
 
   return 0;
